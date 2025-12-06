025b30e79fee3366eedf273960acdf37bd463a01
LBornstein <lior.bornstein@gmail.com>
2025-12-06 16:31:12 +0200

    print centroids func

--- kmeans.c
+++ kmeans.c
@@ -2,18 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
-double calculateDistance(double p[], double q[], int n){
-    int i = 0;
+double calculate_distance(double p[], double q[], int n){
     double sum = 0.0;
-    for (i = 0; i < n; ++i){
-        sum += (p[i]-q[i])*(p[i]-q[i]);
+    for (int i = 0; i < n; i++){
+        sum += (p[i] - q[i]) * (p[i] - q[i]);
     }
     return sqrt(sum);
 }
 
-int main(void ){
+void print_centroids(int k, int dim, double m[k][dim]){
+    for (int i = 0; i < k; i++){
+        for (int j = 0; j < dim; j++){
+            if (j == dim - 1) printf("%.4f", m[i][j]);
+            else printf("%.4f,", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void){
     double p[] = {3.0, 4.0};
     double q[] = {0.0, 0.0};
+    double m[2][2] = {
+        { p[0], p[1] },
+        { q[0], q[1] }
+    };
+
     int n = sizeof(p) / sizeof(p[0]);
-    printf("%f", calculateDistance(p,q,n));
+    printf("%f\n", calculate_distance(p, q, n));
+    print_centroids(2, 2, m);
 }
